075ee640c366cf1f29ad8eacf33c943832811c2b
Adrien5902 <81705101+Adrien5902@users.noreply.github.com>
2022-12-14 17:16:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,52 @@
 class atom {
-    constructor (Z) {
-        this.Z = Z
-    }
-
     getCouches() {
         const limit = {
             s : 2,
             p : 6,
-            d : 10
-            f : 14
-            g : 18
+            d : 10,
+            f : 14,
+            g : 18,
         }
+
+        const couchesList = [
+            "1s","2s","2p","3s","3p","4s","3d","4p","5s","4d","5p","6s","4f","5d","6p","7s","5f","6d","7p"
+        ] 
         
         let remaining = this.Z
         let couches = {}
 
-        for(let couche in couchesList){
-            const couches[couche] = {}
+        let i = 0;
+        while (remaining >= 1){ //tant qu'il y a des e- a répartir
+            let couche = couchesList[i]
 
-            const sousCouches = couchesList[couche] //liste des sous couches dans la couche
+            const limiteSousCouche = limit[couche[1]] //limite sous couche  ex : 2 (pour s), 6 (pour d)...
 
-            for(let j = 0; j < sousCouches.length; j++){                        
-                const sousCouche = sousCouches[j] //lettre sous couche  ex : s, p, d...
-                const limiteSousCouche = limit[sousCouche] //limite sous couche  ex : 2 (pour s), 6 (pour d)...
-
-                couches[couche][sousCouche] = limiteSousCouche
-                remaining -= limiteSousCouche
-                
-                if(remaining < 1){
-                    couches[couche][sousCouche] += remaining
-                    break
-                }
-            }
-                
+            couches[couche] = limiteSousCouche
+            remaining -= limiteSousCouche
+            
             if(remaining < 1){
+                couches[couche] += remaining
                 break
             }
+            i++
         }
         return couches
     }
 
+    constructor (Z) {
+        if(Z < 1 && Z > 118){
+            console.error("Atome inexistant")
+        }else{
+            this.Z = Z
+            this.couches = this.getCouches()
+        }
+    }
+
     couchesString() {
         let str = ""
-        let couches = this.getCouches()
-        for(let couche in couche){
-            for(let sousCouche in couches[couche]){
-                str += "(" + couche+sousCouche+ "<sup>" + couches[couche][sousCouche] + "</sup>)"
-            }
+        let couches = this.couches
+        for(let couche in couches){
+            str += "(" + couche + "<sup>" + couches[couche] + "</sup>)"
         }
         return str
     }
