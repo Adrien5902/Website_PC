b4d900fc614258348d09d30d1c73ea4993d58a94
Adrien5902 <adrien.m5902@gmail.com>
2022-12-16 12:49:16 +0100

    Ajout de commentaires un peu partout

--- main.js
+++ main.js
@@ -1,5 +1,132 @@
-//
+//Liste des élements du tableau périodique
+const elements = [
+    {Z :1, name: "Hydrogène", symbol : "H"},
+    {Z :2, name: "Hélium", symbol : "He"},
+    {Z :3, name: "Lithium", symbol : "Li"},
+    {Z :4, name: "Béryllium", symbol : "Be"},
+    {Z :5, name: "Bore", symbol : "B"},
+    {Z :6, name: "Carbone", symbol : "C"},
+    {Z :7, name: "Azote", symbol : "N"},
+    {Z :8, name: "Oxygène", symbol : "O"},
+    {Z :9, name: "Fluor", symbol : "F"},
+    {Z :10, name: "Néon", symbol : "Ne"},
+    {Z :11, name: "Sodium", symbol : "Na"},
+    {Z :12, name: "Magnésium", symbol : "Mg"},
+    {Z :13, name: "Aluminium", symbol : "Al"},
+    {Z :14, name: "Silicium", symbol : "Si"},
+    {Z :15, name: "Phosphore", symbol : "P"},
+    {Z :16, name: "Soufre", symbol : "S"},
+    {Z :17, name: "Chlore", symbol : "Cl"},
+    {Z :18, name: "Argon", symbol : "Ar"},
+    {Z :19, name: "Potassium", symbol : "K"},
+    {Z :20, name: "Calcium", symbol : "Ca"},
+    {Z :21, name: "Scandium", symbol : "Sc"},
+    {Z :22, name: "Titane", symbol : "Ti"},
+    {Z :23, name: "Vanadium", symbol : "V"},
+    {Z :24, name: "Chrome", symbol : "Cr"},
+    {Z :25, name: "Manganèse", symbol : "Mn"},
+    {Z :26, name: "Fer", symbol : "Fe"},
+    {Z :27, name: "Cobalt", symbol : "Co"},
+    {Z :28, name: "Nickel", symbol : "Ni"},
+    {Z :29, name: "Cuivre", symbol : "Cu"},
+    {Z :30, name: "Zinc", symbol : "Zn"},
+    {Z :31, name: "Gallium", symbol : "Ga"},
+    {Z :32, name: "Germanium", symbol : "Ge"},
+    {Z :33, name: "Arsenic", symbol : "As"},
+    {Z :34, name: "Sélénium", symbol : "Se"},
+    {Z :35, name: "Brome", symbol : "Br"},
+    {Z :36, name: "Krypton", symbol : "Kr"},
+    {Z :37, name: "Rubidium", symbol : "Rb"},
+    {Z :38, name: "Strontium", symbol : "Sr"},
+    {Z :39, name: "Yttrium", symbol : "Y"},
+    {Z :40, name: "Zirconium", symbol : "Zr"},
+    {Z :41, name: "Niobium", symbol : "Nb"},
+    {Z :42, name: "Molybdène", symbol : "Mo"},
+    {Z :43, name: "Technétium", symbol : "Tc"},
+    {Z :44, name: "Ruthénium", symbol : "Ru"},
+    {Z :45, name: "Rhodium", symbol : "Rh"},
+    {Z :46, name: "Palladium", symbol : "Pd"},
+    {Z :47, name: "Argent", symbol : "Ag"},
+    {Z :48, name: "Cadmium", symbol : "Cd"},
+    {Z :49, name: "Indium", symbol : "In"},
+    {Z :50, name: "Étain", symbol : "Sn"},
+    {Z :51, name: "Antimoine", symbol : "Sb"},
+    {Z :52, name: "Tellure", symbol : "Te"},
+    {Z :53, name: "Iode", symbol : "I"},
+    {Z :54, name: "Xénon", symbol : "Xe"},
+    {Z :55, name: "Césium", symbol : "Cs"},
+    {Z :56, name: "Baryum", symbol : "Ba"},
+    {Z :57, name: "Lanthane", symbol : "La"},
+    {Z :58, name: "Cérium", symbol : "Ce"},
+    {Z :59, name: "Praséodyme", symbol : "Pr"},
+    {Z :60, name: "Néodyme", symbol : "Nd"},
+    {Z :61, name: "Prométhium", symbol : "Pm"},
+    {Z :62, name: "Samarium", symbol : "Sm"},
+    {Z :63, name: "Europium", symbol : "Eu"},
+    {Z :64, name: "Gadolinium", symbol : "Gd"},
+    {Z :65, name: "Terbium", symbol : "Tb"},
+    {Z :66, name: "Dysprosium", symbol : "Dy"},
+    {Z :67, name: "Holmium", symbol : "Ho"},
+    {Z :68, name: "Erbium", symbol : "Er"},
+    {Z :69, name: "Thulium", symbol : "Tm"},
+    {Z :70, name: "Ytterbium", symbol : "Yb"},
+    {Z :71, name: "Lutécium", symbol : "Lu"},
+    {Z :72, name: "Hafnium", symbol : "Hf"},
+    {Z :73, name: "Tantale", symbol : "Ta"},
+    {Z :74, name: "Tungstène", symbol : "W"},
+    {Z :75, name: "Rhénium", symbol : "Re"},
+    {Z :76, name: "Osmium", symbol : "Os"},
+    {Z :77, name: "Iridium", symbol : "Ir"},
+    {Z :78, name: "Platine", symbol : "Pt"},
+    {Z :79, name: "Or", symbol : "Au"},
+    {Z :80, name: "Mercure", symbol : "Hg"},
+    {Z :81, name: "Thallium", symbol : "Tl"},
+    {Z :82, name: "Plomb", symbol : "Pb"},
+    {Z :83, name: "Bismuth", symbol : "Bi"},
+    {Z :84, name: "Polonium", symbol : "Po"},
+    {Z :85, name: "Astate", symbol : "At"},
+    {Z :86, name: "Radon", symbol : "Rn"},
+    {Z :87, name: "Francium", symbol : "Fr"},
+    {Z :88, name: "Radium", symbol : "Ra"},
+    {Z :89, name: "Actinium", symbol : "Ac"},
+    {Z :90, name: "Thorium", symbol : "Th"},
+    {Z :91, name: "Protactinium", symbol : "Pa"},
+    {Z :92, name: "Uranium", symbol : "U"},
+    {Z :93, name: "Neptunium", symbol : "Np"},
+    {Z :94, name: "Plutonium", symbol : "Pu"},
+    {Z :95, name: "Américium", symbol : "Am"},
+    {Z :96, name: "Curium", symbol : "Cm"},
+    {Z :97, name: "Berkélium", symbol : "Bk"},
+    {Z :98, name: "Californium", symbol : "Cf"},
+    {Z :99, name: "Einsteinium", symbol : "Es"},
+    {Z :100, name: "Fermium", symbol : "Fm"},
+    {Z :101, name: "Mendélévium", symbol : "Md"},
+    {Z :102, name: "Nobélium", symbol : "No"},
+    {Z :103, name: "Lawrencium", symbol : "Lr"},
+    {Z :104, name: "Rutherfordium", symbol : "Rf"},
+    {Z :105, name: "Dubnium", symbol : "Db"},
+    {Z :106, name: "Seaborgium", symbol : "Sg"},
+    {Z :107, name: "Bohrium", symbol : "Bh"},
+    {Z :108, name: "Hassium", symbol : "Hs"},
+    {Z :109, name: "Meitnérium", symbol : "Mt"},
+    {Z :110, name: "Darmstadtium", symbol : "Ds"},
+    {Z :111, name: "Roentgenium", symbol : "Rg"},
+    {Z :112, name: "Copernicium", symbol : "Cn"},
+    {Z :113, name: "Nihonium", symbol : "Nh"},
+    {Z :114, name: "Flérovium", symbol : "Fl"},
+    {Z :115, name: "Moscovium", symbol : "Mc"},
+    {Z :et, name: "Institut", symbol : "unifié"},
+    {Z :116, name: "Livermorium", symbol : "Lv"},
+    {Z :117, name: "Tennesse", symbol : "Ts"},
+    {Z :de, name: "l", symbol : "Institut"},
+    {Z :118, name: "Oganesson", symbol : "Og"},
+    {Z :et, name: "Institut", symbol : "unifié"}
+]
+
+//Class Atome
 class atom {
+
+    //Couches électroniques
     getCouches() {
         const limit = {
             s : 2,
@@ -34,7 +161,8 @@ class atom {
         return couches
     }
 
-    constructor (Z) {
+    //Constructeur Atom
+    constructor (Z) { //Z = Numéro atomique
         if(Z < 1 && Z > 118){
             console.error("Atome inexistant")
         }else{
@@ -43,6 +171,7 @@ class atom {
         }
     }
 
+    //Renvoie les couches électroniques sous forme de texte
     couchesString() {
         let str = ""
         let couches = this.couches
