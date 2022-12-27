dcd7f5d3f5564438b1421fe753e875f4abdb495c
Adrien5902 <adrien.m5902@gmail.com>
2022-12-27 19:07:23 +0100

    added legende and optimized color function

--- atom/tableauEl.js
+++ atom/tableauEl.js
@@ -13,44 +13,53 @@ for(let i = 6; i <= 7; i++){
     tableauEl.innerHTML += '<tr ce="' + i + 'bis"></tr>'
 }
 
+function colorByBloc(bloc, period){
+    let rgb = false
+    let color
+
+    if(bloc === "s"){
+        color = 255 - ((period + 1) ** 1.3)
+        rgb = 'rgb(' + color + ', ' + color + ', 255)';
+    }else if(bloc === "p"){
+        color = 255 - (period ** 2.2 * 3)
+        rgb = 'rgb(' + color + ', 255, ' + color + ')';
+    }else if(bloc === "d"){
+        color = 255 - (period ** 3 /1.8)
+        rgb = 'rgb(255 , 255, ' + color + ')';
+    }else if(bloc === "f"){
+        color = (255 - (period ** 2.9) + 100)
+        rgb = "rgb(255, " + color + ", " + color +")"
+    }
+    
+    return rgb
+}
+
 for(let el of elements) {
     let atome = new atom(el.Z)
 
-    let dernièreCouche = Object.keys(atome.couches)[Object.keys(atome.couches).length - 1]
-
     let td = document.createElement('td');
     td.innerHTML = atome.cell()
-    
+
     let tr
-    if(dernièreCouche[1] != "f"){
+
+    if(atome.bloc != "f"){
         tr = document.querySelector("tr[ce='"+ atome.période[0] +"']");
-        let rgb
-
-        if(dernièreCouche[1] === "s"){
-            let color = 255 - ((atome.période + 1) ** 1.3)
-            rgb = 'rgb(' + color + ', ' + color + ', 255)';
-        }else if(dernièreCouche[1] === "p"){
-            let color = 255 - (atome.période ** 2.2 * 3)
-            rgb = 'rgb(' + color + ', 255, ' + color + ')';
-        }else if(dernièreCouche[1] === "d"){
-            let color = 255 - (atome.période ** 3 /1.8)
-            rgb = 'rgb(255 , 255, ' + color + ')';
-        }
-
-        td.childNodes[0].style.backgroundColor = rgb
     }else{
         tr = document.querySelector("tr[ce='"+ atome.période[0] +"bis']");
-        let color = (255 - (atome.période ** 2.9) + 100)
-        let rgb = "rgb(255, " + color + ", " + color +")"
-        td.childNodes[0].style.backgroundColor = rgb
+    }
+
+    let color = colorByBloc(atome.bloc, atome.période)
+    if(color){
+        td.childNodes[0].style.backgroundColor = color
     }
 
     if(atome.couches["2p"] === 1 || atome.couches["3p"] === 1){
         tr.innerHTML += ('<td colspan="10"></td>')
     }
+
     tr.appendChild(td);
 
-    if(dernièreCouche[1] == "s" && atome.couches[atome.période + "s"] == 2){
+    if(atome.bloc == "s" && atome.couches[atome.période + "s"] == 2){
         let whiteSpaceGb = (255 - (atome.période ** 2.9) + 100)
         let whiteSpaceRgb = "rgb(255, " + whiteSpaceGb + ", " + whiteSpaceGb +")"
         tr.innerHTML += ("<td p='" + atome.période + "'>&nbsp;</td>");
@@ -62,4 +71,19 @@ for(let el of elements) {
     if(atome.couches["1s"] === 1){
         tr.innerHTML += ('<td colspan="17"></td>')
     }
+}
+
+let legende = document.getElementById("legende")
+for(let bloc of Object.keys(couchesLimit)){
+    let tr = document.createElement("tr")
+    legende.appendChild(tr)
+    tr.setAttribute["bloc", bloc]
+
+    let colorBox = document.createElement("td")
+    tr.appendChild(colorBox)
+    colorBox.style.backgroundColor = colorByBloc(bloc, "7")
+
+    let blocTd = document.createElement("td")
+    tr.appendChild(blocTd)
+    blocTd.innerHTML = "Bloc " + bloc.toUpperCase()
 }
