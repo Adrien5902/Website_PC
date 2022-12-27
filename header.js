26e60a489fbd25f701e211faa69abffc719620cc
Adrien5902 <adrien.m5902@gmail.com>
2022-12-27 20:20:32 +0100

    cwx

--- header.js
+++ header.js
@@ -1,9 +1,4 @@
 //En-tête sur toutes les pages
-
-let header = document.createElement('header');
-body = document.querySelector('body');
-body.insertBefore(header, body.firstChild);
-
 let scripts = document.querySelectorAll('script')
 let scriptSrc = ""
 scripts.forEach(function(script) {
@@ -12,5 +7,16 @@ scripts.forEach(function(script) {
     }
 })
 
+//Head
+let favicon = '<link rel="shortcut icon" href="' + scriptSrc + 'favicon.ico" type="image/x-icon"/>'
+document.head.innerHTML += favicon
+
+let style = '<link rel="stylesheet" href="' + scriptSrc + 'style.css">'
+document.head.innerHTML += style
+
+//Header
+let header = document.createElement('header');
+body = document.querySelector('body');
+body.insertBefore(header, body.firstChild);
 //Je sais que c'est pas propre mais on peut pas fetch des fichiers html depuis JS sur un fichier local
 header.innerHTML = '<a class="unlink" href="' + scriptSrc + 'index.html"><img src="' + scriptSrc + 'logo.png" alt="[Nom du site]"></a> <br> <a class="unlink" href="' + scriptSrc + 'atom/index.html">Atomes</a> <a class="unlink" href="' + scriptSrc + 'molecules/index.html">Molécules</a> <a class="unlink" href="' + scriptSrc + 'elec/index.html">Électricité</a>';
