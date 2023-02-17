c377cff15f10d3ad046c136bcf9945c9c998aa22
Adrien5902 <adrien.m5902@gmail.com>
2023-02-18 00:05:47 +0100

    moved script

---
+++ pages.js
@@ -0,0 +1,20 @@
+let pDiv = document.getElementById("pages")
+
+let scriptsInPage = document.querySelectorAll('script')
+let pagesScriptSrc = ""
+scriptsInPage.forEach(function(script) {
+    if(script.src.includes('pages.js')){
+        pagesScriptSrc = script.src.replace('pages.js', '');
+    }
+})
+
+for(let page of pages){
+    let btn = document.createElement("a")
+    pDiv.appendChild(btn)
+    btn.style.backgroundImage = "url(" + pagesScriptSrc + "imgs/pages/" + page.img + ")"
+    btn.href = page.href
+    
+    let txt = document.createElement("span")
+    btn.appendChild(txt)
+    txt.innerHTML = page.text
+}
